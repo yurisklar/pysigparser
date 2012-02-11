5e16943bc23293e8c92fd12d1a962e9902662787
Yuri Sklar <yuri.sklar@gmail.com>
2012-02-11 19:47:16 +0200

    Adding simple parser for websites and emails. TODO: parse one more item, docs

--- sigparser/sigparser.py
+++ sigparser/sigparser.py
@@ -1,17 +1,25 @@
 import re
 
-# todo: not completed - use decorators
+# changed the idea to use decorators
+# because they would work here with input
 
 class SignatureParser:
     """
     Parses text input to get information about a person
     """
 
-    def _get_website(self, input, result):
-        result["URL"] = "http://example.com"
-        return result
+    def __init__(self):
+        self.regexps = dict(
+            email = re.compile(r'([\w\-\.]+@[\w\-\.]+\.[\w\-]+)'),
+            website = re.compile(r'(https?://[^\s]+)')
+        )
 
     def get_information(self, input):
         result = dict()
-        result = self._get_website(input, result)
+        for look_for in self.regexps:
+            matched = self.regexps[look_for].findall(input)
+            if matched:
+                result[look_for] = matched
+
         return result
+
